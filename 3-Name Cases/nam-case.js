417f0ad38a2d9daff6aa11d4b40d3a9b77ff31ec
Aanay555 <160274256+Aanay555@users.noreply.github.com>
2024-04-19 00:09:24 +0500

    your file has been updated

--- 3-Name Cases/nam-case.js
+++ 3-Name Cases/nam-case.js
@@ -1,6 +1,4 @@
 #! usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // Q3
 // // Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
 var myName = "Uzma riaz";
@@ -10,3 +8,4 @@ console.log("uppercase:", myName.toLocaleUpperCase());
 console.log("lowercase:", myName.toLocaleLowerCase());
 //  //titlecase//
 console.log("titlecase:", myName.replace(/\b\w/g, function (c) { return c.toUpperCase(); }));
+export {};
