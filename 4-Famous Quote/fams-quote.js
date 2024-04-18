38ec85aa2a1d62b58b3e21ae815eaee9861b3238
Aanay555 <160274256+Aanay555@users.noreply.github.com>
2024-04-19 00:38:03 +0500

    print message

--- 4-Famous Quote/fams-quote.js
+++ 4-Famous Quote/fams-quote.js
@@ -1,8 +1,7 @@
 #! usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // q4
 // Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
 let author1 = 'Allama Iqbal';
 let quote1 = "Nations are born in the hearts of poets, they prosper and die in the hands of politicians.";
 console.log("".concat(author1, ", \"").concat(quote1, "\""));
+export {};
